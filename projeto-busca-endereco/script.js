86f287aed99f33fac6e8341406d78afc6cd7e633
vinyazevedo2 <133170592+vinyazevedo2@users.noreply.github.com>
2024-03-07 19:29:07 -0300

    arquivo completo

---
+++ projeto-busca-endereco/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+    $('body').on('click', '#consultar', function(){
+        consultaCep();
+    });
+    function consultaCep(){
+        const cep = $('#cep').val();
+
+        if(cep.length !== 8){
+            alert('Digite o cep corretamente');
+            return false;
+        }
+
+        $.ajax({
+            url: 'https://viacep.com.br/ws/'+cep+'/json/', 
+            type: 'GET',
+            dataType: 'json',
+            success: function(data){
+                console.log(data);
+            }
+        })
+    }
+
+})
